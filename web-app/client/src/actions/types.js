60858ca79a9b05d1f47349a210ea10c907982288
Andrew Forrester <awforrester@gmail.com>
2021-09-10 13:12:10 -0700

    Add action to set query input.

--- web-app/client/src/actions/types.js
+++ web-app/client/src/actions/types.js
@@ -11,6 +11,7 @@ export const GET_FILE_START   = 'GET_FILE_START'
 export const GET_FILE_SUCCESS = 'GET_FILE_SUCCESS'
 export const GET_FILE_FAIL    = 'GET_FILE_FAIL'
 
+export const QUERY_INPUT_SET = 'QUERY_INPUT_SET'
 export const QUERY_START   = 'QUERY_START'
 export const QUERY_SUCCESS = 'QUERY_SUCCESS'
 export const QUERY_FAIL    = 'QUERY_FAIL'
